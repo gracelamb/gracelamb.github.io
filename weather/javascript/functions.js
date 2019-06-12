60b0593f0aa07a9409f756c3d26cc1a38baa9e08
gracelamb <you@example.com>
2019-06-12 13:39:07 -0600

    Wind Dial

--- weather/javascript/functions.js
+++ weather/javascript/functions.js
@@ -29,6 +29,51 @@ function buildWC(speed, temp){
 }
 
 // Wind Pointer Function
+function windDial(direction){
+
+    // Get the wind dial container
+    const dial = document.getElementById("dial");
+
+    // Determine the dial class
+    switch (direction){
+        case "North":
+        case "N":
+         dial.setAttribute("class", "n"); //"n" is the CSS rule selector
+         break;
+        case "NE":
+        case "NNE":
+        case "ENE":
+         dial.setAttribute("class", "ne");
+         break;
+        case "NW":
+        case "NNW":
+        case "WNW":
+         dial.setAttribute("class", "nw");
+         break;
+        case "South":
+        case "S":
+         dial.setAttribute("class", "s");
+         break;
+        case "SE":
+        case "SSE":
+        case "ESE":
+         dial.setAttribute("class", "se");
+         break;
+        case "SW":
+        case "SSW":
+        case "WSW":
+         dial.setAttribute("class", "sw");
+         break;
+        case "East":
+        case "E":
+         dial.setAttribute("class", "e");
+         break;
+        case "West":
+        case "W":
+         dial.setAttribute("class", "w");
+         break;
+       }
+}
 
 
 

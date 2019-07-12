00334b28e84d2762190c74b80a64995ef974ff71
gracelamb <you@example.com>
2019-07-12 13:03:38 -0600

    Template

---
+++ acme/js/functions.js
@@ -0,0 +1,33 @@
+'use strict;'
+console.log("My JavaScript is being read.")
+getData();
+
+// getData function
+function getData(){
+  let URL = "/acme/js/acme.json"
+
+  // Get the data from the Json file
+  fetch(URL) 
+  .then(function(response){
+    if(response.ok){ 
+     return response.json(); 
+    } 
+    throw new ERROR('Response not OK.');
+  })
+  .then(function (data) { 
+    console.log(data);
+    console.log(Object.keys(data));
+    let products = Object.keys(data);
+    document.getElementById('nav-list').innerHTML += buildNav(products);
+})}
+
+
+// buildNav funcion
+function buildNav(array){
+  let items = '<li><a href="#">Home</a></li>';
+  for (let i = 0, x = array.length; i < x; i++){
+      items += '<li><a href="#">' + array[i] + '</a><li>'
+     }
+     console.log(items);
+     return items;
+}
